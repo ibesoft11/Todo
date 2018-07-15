c66eea1f441cd08171cdfb3706d960b169ad222c
Ibesoft11 <ibesoft11@gmail.com>
2018-07-15 02:46:56 +0100

    added resolvers for updating and deleting items on our todo list

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const Todo = mongoose.model("Todo",{
 const typeDefs = `
   type Query {
     hello(name: String): String!
+    todos: [Todo]
   }
   type Todo {
       id: ID!
@@ -18,24 +19,35 @@ const typeDefs = `
       complete: Boolean!
   }
   type Mutation {
-      createTodo(text: String!): Todo!
+      createTodo(text: String!): Todo!,
+      updateTodo(id: ID!, complete: Boolean!): Boolean,
+      deleteTodo(id: ID!): String
   }
 `
 
 const resolvers = {
   Query: {
     hello: (_, { name }) => `Hello ${name || 'World'}`,
+    todos: ()=> Todo.find()
   },
   Mutation: {
       createTodo: async (_, { text }) => {
           const todo = new Todo({text, complete:false});
           await todo.save();
           return todo;
+      },
+      updateTodo: async (_, {id, complete}) =>{
+          await Todo.findByIdAndUpdate(id, {complete});
+          return true;
+      },
+      deleteTodo: async (_, {id}) => {
+          await Todo.findByIdAndRemove(id);
+          return "item deleted";
       }
   }
 }
 
 const server = new GraphQLServer({ typeDefs, resolvers })
-mongoose.Connection.once("open", function(){
+mongoose.connection.once("open", function(){
     server.start(() => console.log('Server is running on localhost:4000'))
 });
