91a7038eb2f3d5bb1589420d0f863577d889d87e
Ibesoft11 <ibesoft11@gmail.com>
2018-07-15 06:38:17 +0100

    Added code to handle key events

--- src/form.js
+++ src/form.js
@@ -2,12 +2,33 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 
 export default class Form extends React.Component{
+    state = {
+        text: ""
+    };
+
+    handleChange = (e) => {
+        const newText = e.target.value;
+        this.setState({
+            text: newText
+        });
+    };
+    handleKeyDown = (e) => {
+        if (e.key === 'Enter'){
+            this.props.submit(this.state.text);
+            this.setState({text: ""});
+        }
+    };
+
     render(){
+        const {text} = this.state;
         return (
             <TextField
             label="add item to the list..."
             margin="normal"
             fullWidth
+            value = {text}
+            onChange = {this.handleChange}
+            onKeyDown = {this.handleKeyDown}
             />
         );
     }
