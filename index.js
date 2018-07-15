14a5e1140f0108875995047635f49ee7c883310c
Ibesoft11 <ibesoft11@gmail.com>
2018-07-15 02:07:37 +0100

    added connections for graphql-yoga and mongoose

---
+++ index.js
@@ -0,0 +1,25 @@
+const { GraphQLServer } = require('graphql-yoga')
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/todoApp");
+
+const Todo = mongoose.model("Todo",{
+    text: String,
+    complete: Boolean
+});
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => `Hello ${name || 'World'}`,
+  },
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers })
+mongoose.Connection.once("open", function(){
+    server.start(() => console.log('Server is running on localhost:4000'))
+});
